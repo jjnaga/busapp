99622a9a512cb20878f00b6a939be5db394d4fad
Jeremy Nagahama <naga@hawaii.edu>
2024-06-11 12:40:34 -1000

    setup typeorm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 import morgan from 'morgan';
 import helloRoutes from '@routes/helloRoutes';
+import { AppDataSource } from '@typeorm/typeorm';
+import vehiclesRoutes from '@routes/vehiclesRoutes';
 
+(async () => {
+  // Initialize TypeORM
+  await AppDataSource.initialize();
+
+  // Initialize Express
 const app = express();
 const port = process.env.EXPRESS_PORT || 3000;
 
-// Does logging
+  // Express Logging
 app.use(morgan('combined'));
 
+  // Setup routes
 app.use('/hello', helloRoutes);
 
+  // Listen
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+})();
