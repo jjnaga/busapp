f04c312c69bf80114c159d031ed6335a0b281def
Jeremy Nagahama <naga@hawaii.edu>
2024-06-11 21:38:36 -1000

    add header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
