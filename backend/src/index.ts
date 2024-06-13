724a712f7f3ddde114775a312412f7adf865cded
Jeremy Nagahama <naga@hawaii.edu>
2024-06-12 17:21:26 -1000

    setup fetch to api and display in main component

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import helloRoutes from '@routes/helloRoutes';
 import { AppDataSource } from '@typeorm/typeorm';
 import vehiclesRoutes from '@routes/vehiclesRoutes';
 
 (async () => {
+  const corsOptions = { origin: ['http://localhost:4200', 'https://nagahama-group.com'] };
+
   // Initialize TypeORM
   await AppDataSource.initialize();
 
   // Initialize Express
   const app = express();
+
+  // Enable CORS
+  app.use(cors(corsOptions));
+
   const port = process.env.EXPRESS_PORT || 3000;
 
   // Express Logging
