22ae4ca64b32b1a26f0301336bfd2dc319e8ec0e
Jeremy Nagahama <naga@hawaii.edu>
2024-06-15 19:21:21 -1000

    cleanup frontend and only get relevant vehicle data

--- backend/src/logic/vehicles.ts
+++ backend/src/logic/vehicles.ts
@@ -1,10 +1,18 @@
 import { AppDataSource } from '@typeorm/typeorm';
 import { Vehicle } from '@typeorm/entities/Vehicle';
+import { IsNull, MoreThanOrEqual, Not } from 'typeorm';
 
 export const fetchAllVehicles = async () => {
   try {
     const vehicleRepo = AppDataSource.getRepository(Vehicle);
-    const vehicles = await vehicleRepo.find();
+    const twentyFourHoursAgo = new Date();
+    twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24);
+
+    const vehicles = await vehicleRepo.findBy({
+      tripId: Not(IsNull()),
+      heartbeat: MoreThanOrEqual(twentyFourHoursAgo),
+    });
+
     return vehicles;
   } catch (err) {
     throw new Error(err instanceof Error ? err.message : String(err));
