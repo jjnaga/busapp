d873bea1fb1a4f4773ff24a3637f0c190a55255b
Jeremy Nagahama <naga@hawaii.edu>
2024-06-22 14:47:52 -1000

    Update job manager. Move from bullmq to redis stream

--- services/manager/src/index.ts
+++ services/manager/src/index.ts
@@ -1,55 +1,74 @@
-import { addJob, clearJob, createQueue } from './utils/bullmq';
-import { JobsOptions } from 'bullmq';
+import redisClient from '@utils/redisClient';
 
-const loadRecurringJob = async (jobName: string, jobOptions: JobsOptions) => {
-  // Clear any exisiting jobs.
-  await clearJob(jobName, jobOptions);
-  // Add reoccurring job
-  await addJob(jobName, {}, { ...jobOptions, jobId: `${jobName}-repeating` });
+const REDIS_STREAM_NAME = process.env.REDIS_STREAM_NAME;
+const REDIS_CONSUMER_GROUPS = process.env.REDIS_CONSUMER_GROUPS;
+const REDIS_JOB_API = process.env.REDIS_JOB_API;
+const REDIS_JOB_GTFS = process.env.REDIS_JOB_GTFS;
 
-  // Initial run of job.
-  await addJob(jobName);
+if (!REDIS_STREAM_NAME) {
+  throw new Error(
+    `Init Error. REDIS_STREAM_NAME is not defined as an environment variable`
+  );
+}
+
+if (!REDIS_CONSUMER_GROUPS) {
+  throw new Error(
+    `Init Error. REDIS_JOB_BUS_API_ETL is not defined as an environment variable`
+  );
+}
+
+if (!REDIS_JOB_API) {
+  throw new Error(
+    `Init Error. REDIS_JOB_API is not defined as an environment variable`
+  );
+}
+
+const addJob = async (jobName: string, jobData: any = {}) => {
+  const jobId = `${Date.now()}`;
+
+  console.log(`[${REDIS_STREAM_NAME}] Adding job. ID: ${jobId}`);
+
+  await redisClient.xadd(
+    REDIS_STREAM_NAME,
+    '*',
+    'jobName',
+    jobName,
+    'jobData',
+    JSON.stringify(jobData),
+    'jobId',
+    jobId
+  );
+  return jobId;
 };
 
-const main = () => {
-  const queueName = process.env.BULL_QUEUE_NAME;
-  const host = process.env.BULL_HOST;
-  const port = process.env.BULL_PORT;
-  const jobBusApiEtl = process.env.BULL_JOB_BUS_API_ETL;
+const loadRecurringJob = async (
+  jobName: string,
+  jobData: any = {},
+  interval: number
+) => {
+  console.log(`Creating recurring job '${jobName}' with interval ${interval}`);
+  setInterval(async () => {
+    await addJob(jobName, jobData);
+  }, interval);
+};
 
-  if (!queueName) {
-    throw new Error(
-      `Queue Init Error. BULL_QUEUE_NAME is not defined in the environment variables`
-    );
+const main = async () => {
+  for (const consumerGroup of REDIS_CONSUMER_GROUPS.split(',')) {
+    console.log('Creating consumer group: ', consumerGroup);
+    // Create consumer group
+    try {
+      await redisClient.xgroup(
+        'CREATE',
+        REDIS_STREAM_NAME,
+        consumerGroup,
+        '0',
+        'MKSTREAM'
+      );
+    } catch (err) {} // Error thrown if group exists. Ignore error.
   }
-
-  if (!jobBusApiEtl) {
-    throw new Error(
-      `Queue Init Error. BULL_JOB_BUS_API_ETL is not defined in the environment variables`
-    );
-  }
-
-  if (!host) {
-    throw new Error(
-      `Queue Init Error. BULL_HOST is not defined in the environment variables`
-    );
-  }
-
-  if (!port) {
-    throw new Error(
-      `Queue Init Error. BULL_PORT is not defined in the environment variables`
-    );
-  }
-
-  // Create bullmq queue.
-  createQueue(queueName, host, port);
 
   // Load all jobs.
-  loadRecurringJob(jobBusApiEtl, {
-    repeat: {
-      every: 10 * 1000,
-    },
-  });
+  loadRecurringJob(REDIS_JOB_API, {}, 10000);
 };
 
-main();
+main().catch(console.error);
