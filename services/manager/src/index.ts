fffaeaf83ee0a1bf6919668723b8bbd52b6f21d3
Jeremy Nagahama <naga@hawaii.edu>
2024-06-23 11:20:05 -1000

    move bus api worker from bullmq to redis stream

--- services/manager/src/index.ts
+++ services/manager/src/index.ts
@@ -1,74 +1,90 @@
 import redisClient from '@utils/redisClient';
 
-const REDIS_STREAM_NAME = process.env.REDIS_STREAM_NAME;
-const REDIS_CONSUMER_GROUPS = process.env.REDIS_CONSUMER_GROUPS;
-const REDIS_JOB_API = process.env.REDIS_JOB_API;
-const REDIS_JOB_GTFS = process.env.REDIS_JOB_GTFS;
+const REDIS_JOB_API_NAME = process.env.REDIS_JOB_API_NAME;
+const REDIS_JOB_GTFS_NAME = process.env.REDIS_JOB_GTFS_NAME;
+const REDIS_JOBS_MAX_LENGTH = process.env.REDIS_JOBS_MAX_LENGTH;
+const REDIS_STREAM_API_NAME = process.env.REDIS_STREAM_API_NAME;
+const REDIS_STREAM_GTFS_NAME = process.env.REDIS_STREAM_GTFS_NAME;
+let shutdown = false;
 
-if (!REDIS_STREAM_NAME) {
-  throw new Error(
-    `Init Error. REDIS_STREAM_NAME is not defined as an environment variable`
-  );
+if (
+  !REDIS_JOB_API_NAME ||
+  !REDIS_JOB_GTFS_NAME ||
+  !REDIS_JOBS_MAX_LENGTH ||
+  !REDIS_STREAM_API_NAME ||
+  !REDIS_STREAM_GTFS_NAME
+) {
+  throw new Error(`Init Error. A environment variable not defined.`);
 }
 
-if (!REDIS_CONSUMER_GROUPS) {
-  throw new Error(
-    `Init Error. REDIS_JOB_BUS_API_ETL is not defined as an environment variable`
-  );
-}
+const setupShutdownHandlers = () => {
+  process.on('SIGTERM', () => {
+    console.log('Received SIGTERM. Starting graceful shutdown.');
+    shutdown = true;
+  });
 
-if (!REDIS_JOB_API) {
-  throw new Error(
-    `Init Error. REDIS_JOB_API is not defined as an environment variable`
-  );
-}
+  process.on('SIGINT', () => {
+    console.log('Received SIGINT. Starting graceful shutdown.');
+    shutdown = true;
+  });
+};
 
-const addJob = async (jobName: string, jobData: any = {}) => {
-  const jobId = `${Date.now()}`;
-
-  console.log(`[${REDIS_STREAM_NAME}] Adding job. ID: ${jobId}`);
+const addJob = async (streamName: string, jobType: string) => {
+  console.log(`[${streamName}] Adding job '${jobType}'`);
 
   await redisClient.xadd(
-    REDIS_STREAM_NAME,
+    streamName,
+    'MAXLEN',
+    '~',
+    REDIS_JOBS_MAX_LENGTH,
     '*',
-    'jobName',
-    jobName,
-    'jobData',
-    JSON.stringify(jobData),
-    'jobId',
-    jobId
+    'jobType',
+    jobType
   );
-  return jobId;
 };
 
 const loadRecurringJob = async (
+  streamName: string,
   jobName: string,
-  jobData: any = {},
   interval: number
 ) => {
   console.log(`Creating recurring job '${jobName}' with interval ${interval}`);
-  setInterval(async () => {
-    await addJob(jobName, jobData);
+  addJob(streamName, jobName);
+  return setInterval(async () => {
+    await addJob(streamName, jobName);
   }, interval);
 };
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const main = async () => {
-  for (const consumerGroup of REDIS_CONSUMER_GROUPS.split(',')) {
-    console.log('Creating consumer group: ', consumerGroup);
-    // Create consumer group
-    try {
-      await redisClient.xgroup(
-        'CREATE',
-        REDIS_STREAM_NAME,
-        consumerGroup,
-        '0',
-        'MKSTREAM'
-      );
-    } catch (err) {} // Error thrown if group exists. Ignore error.
-  }
+  setupShutdownHandlers();
 
   // Load all jobs.
-  loadRecurringJob(REDIS_JOB_API, {}, 10000);
+  let interval1 = await loadRecurringJob(
+    REDIS_STREAM_API_NAME,
+    REDIS_JOB_API_NAME,
+    10000
+  );
+  let interval2 = await loadRecurringJob(
+    REDIS_STREAM_GTFS_NAME,
+    REDIS_JOB_GTFS_NAME,
+    3600000
+  );
+
+  // Listen for shutdown and close connections.
+  while (!shutdown) {
+    await sleep(1000);
+  }
+
+  // Shutdown
+  console.log('Shutting Down: Ending intervals');
+  clearInterval(interval1);
+  clearInterval(interval2);
+  console.log('Shutting Down: Closing Redis connection');
+  redisClient.disconnect();
+
+  console.log('Shutting Down: bye');
 };
 
 main().catch(console.error);
