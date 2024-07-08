910230945a81c0dd09586b44d7e45768b4b41588
Jeremy Nagahama <naga@hawaii.edu>
2024-07-07 14:16:04 -1000

    add logic to header and abstract variables to rxjs service

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouteService } from '../../core/services/routes.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faStar, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faSearch, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
+import { sideBarModes } from '../../core/models/global.model';
+import { Subscription } from 'rxjs';
+import { UserDataService } from '../../core/services/user-data.service';
 
 @Component({
   selector: 'header-component',
@@ -15,23 +18,49 @@ export class HeaderComponent implements OnInit {
   links: { path: string; label: string }[] = [];
   faStar = faStar;
   faSearch = faSearch;
+  faXmark = faXmark;
   searchResult: string = '';
   showSidebar: boolean = false;
+  sidebarMode: sideBarModes = null;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor(private routeService: RouteService) {}
+  constructor(
+    private routeService: RouteService,
+    private userDataService: UserDataService
+  ) {}
 
   ngOnInit(): void {
     const routes = this.routeService.getRoutes();
-    this.links = routes.map((route) => ({
-      path: route.path || '',
-      label: route.path
-        ? route.path.charAt(0).toUpperCase() + route.path.slice(1)
-        : 'Home',
-    }));
+
+    this.subscriptions.add(
+      this.userDataService.searchResult$.subscribe(
+        (result) => (this.searchResult = result)
+      )
+    );
+
+    this.subscriptions.add(
+      this.userDataService.showSidebar$.subscribe(
+        (sidebar) => (this.showSidebar = sidebar)
+      )
+    );
+
+    this.subscriptions.add(
+      this.userDataService.sidebarMode$.subscribe(
+        (sidebarMode) => (this.sidebarMode = sidebarMode)
+      )
+    );
   }
 
-  onSearch() {
-    console.log('typed: ', this.searchResult);
-    this.showSidebar = this.searchResult.length > 0;
+  onSearchResultChange() {
+    this.userDataService.setSearchResult(this.searchResult);
   }
+
+  toggleMode = (mode: sideBarModes) => {
+    this.userDataService.setSidebarMode(mode);
+    this.userDataService.updateShowSidebar();
+  };
+
+  onXmarkClick = () => {
+    this.userDataService.setSidebarMode(null);
+  };
 }
