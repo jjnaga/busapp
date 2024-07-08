6996142e832e6a88b8236d7ef3729f2cb964a065
Jeremy Nagahama <naga@hawaii.edu>
2024-07-07 18:12:23 -1000

    optimize markers by only showing at specified zoom level and only show markers in bounds of map

--- frontend/src/app/shared/components/google-map/google-map.component.ts
+++ frontend/src/app/shared/components/google-map/google-map.component.ts
@@ -11,20 +11,25 @@ import {
   Vehicles,
 } from '../../../core/models/global.model';
 import { GoogleMap, MapAdvancedMarker } from '@angular/google-maps';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { UserDataService } from '../../../core/services/user-data.service';
 
 @Component({
   selector: 'google-map-component',
   templateUrl: './google-map.component.html',
   standalone: true,
-  imports: [GoogleMap, MapAdvancedMarker, AsyncPipe],
+  imports: [GoogleMap, MapAdvancedMarker, AsyncPipe, CommonModule],
 })
 export class GoogleMapComponent implements OnInit, OnDestroy {
+  MIN_ZOOM_LEVEL_FOR_MARKERS = 16; // Adjust this value as needed
+
   private subscriptions: Subscription = new Subscription();
   private destroy$ = new Subject<void>();
   map: google.maps.Map | null = null;
-  markers$ = new BehaviorSubject<Marker[]>([]);
+  stopMarkers$ = new BehaviorSubject<Marker[]>([]);
+  vehicleMarkers$ = new BehaviorSubject<Marker[]>([]);
+  visibleStopMarkers$ = new BehaviorSubject<Marker[]>([]);
+
   mapOptions: google.maps.MapOptions = {
     mapId: '53da1ad002655c53',
     center: { lat: 21.3069, lng: -157.8583 },
@@ -62,75 +67,106 @@ export class GoogleMapComponent implements OnInit, OnDestroy {
   }
 
   private subscribeToData() {
-    combineLatest([
-      this.vehiclesService.getVehiclesObservable(),
-      this.stopsService.getStopsObservable(),
-    ])
-      .pipe(
-        takeUntil(this.destroy$),
-        map(([vehicles, stops]) => this.createMarkers(vehicles, stops))
-        // map(([vehicles, stops]) => this.createMarkers(vehicles))
-      )
-      .subscribe((markers) => {
-        this.markers$.next(markers);
-        // if (markers.length > 0) {
-        //   this.center = markers[0].position; // Center map on first marker
-        // }
-      });
+    this.subscriptions.add(
+      this.vehiclesService.vehicles$
+        .pipe(
+          takeUntil(this.destroy$),
+          map((vehicles) => this.createVehicleMarkers(vehicles))
+        )
+        .subscribe((vehicleMarkers) =>
+          this.vehicleMarkers$.next(vehicleMarkers)
+        )
+    );
+
+    this.subscriptions.add(
+      this.stopsService.stops$
+        .pipe(
+          takeUntil(this.destroy$),
+          map((stops) => this.createStopMarkers(stops))
+        )
+        .subscribe((stopMarkers) => this.stopMarkers$.next(stopMarkers))
+    );
   }
 
-  private createMarkers(
-    vehicles?: Map<string, Vehicle>,
-    stops?: Stop[]
-  ): Marker[] {
-    let busMarkers: any[] = [];
-    let stopMarkers: any[] = [];
+  private createStopMarkers(stops?: Stop[]): Marker[] {
+    if (!stops) return [];
+    return stops
+      .filter(
+        (stop): stop is Stop & { stopLat: number; stopLon: number } =>
+          stop.stopLat !== null &&
+          stop.stopLon !== null &&
+          !isNaN(stop.stopLat) &&
+          !isNaN(stop.stopLon)
+      )
+      .map((stop) => ({
+        id: `stop-${stop.stopCode}`,
+        stopCode: stop.stopCode,
+        position: { lat: stop.stopLat, lng: stop.stopLon },
+        title: stop.stopName || 'Unnamed Stop',
+        type: 'stop',
+      }));
+  }
 
-    if (vehicles) {
-      busMarkers = Array.from(vehicles.values()).map((vehicle) => {
-        const imgTag = document.createElement('img');
-        imgTag.src = 'assets/bus.png';
-        imgTag.width = 50;
-        imgTag.height = 50;
+  private createVehicleMarkers(vehicles?: Map<string, Vehicle>): Marker[] {
+    if (!vehicles) return [];
+    return Array.from(vehicles.values()).map((vehicle) => ({
+      id: `bus-${vehicle.busNumber}`,
+      position: { lat: vehicle.latitude, lng: vehicle.longitude },
+      title: `Bus ${vehicle.busNumber}`,
+      type: 'bus',
+      content: this.createBusMarkerContent(),
+    }));
+  }
 
-        return {
-          position: { lat: vehicle.latitude, lng: vehicle.longitude },
-          title: `Bus ${vehicle.busNumber}`,
-          type: 'bus',
-          content: imgTag,
-          // This causes issues?
-          // options: {
-          //   // icon: imgTag,
-          //   size: new google.maps.Size(32, 32),
-          // },
-        };
-      });
-    }
+  private createBusMarkerContent(): HTMLImageElement {
+    const imgTag = document.createElement('img');
+    imgTag.src = 'assets/bus.png';
+    imgTag.width = 50;
+    imgTag.height = 50;
+    return imgTag;
+  }
 
-    if (stops) {
-      stopMarkers = stops
-        .filter(
-          (stop): stop is Stop & { stopLat: number; stopLon: number } =>
-            stop.stopLat !== null &&
-            stop.stopLon !== null &&
-            !isNaN(stop.stopLat) &&
-            !isNaN(stop.stopLon)
-        )
-        .map((stop) => ({
-          stopCode: stop.stopCode,
-          position: { lat: stop.stopLat, lng: stop.stopLon },
-          title: stop.stopName || 'Unnamed Stop',
-          type: 'stop',
-        }));
-    }
-
-    return [...busMarkers, ...stopMarkers];
+  trackByMarkerId(index: number, marker: Marker): string {
+    return marker.id;
   }
 
   onMapReady(map: google.maps.Map) {
     this.map = map;
     this.map.setCenter({ lat: 21.2968, lng: -157.8531 });
     this.map.setZoom(14);
+
+    this.map.addListener('zoom_changed', () => {
+      this.updateVisibleMarkers();
+    });
+
+    this.map.addListener('bounds_changed', () => {
+      this.updateVisibleMarkers();
+    });
+
+    this.updateVisibleMarkers();
+  }
+  private updateVisibleMarkers() {
+    if (!this.map) return;
+
+    const currentZoom = this.map.getZoom();
+    const bounds = this.map.getBounds();
+
+    if (
+      currentZoom !== undefined &&
+      bounds &&
+      currentZoom >= this.MIN_ZOOM_LEVEL_FOR_MARKERS
+    ) {
+      const visibleStopMarkers = this.stopMarkers$.value.filter((marker) =>
+        bounds.contains(marker.position)
+      );
+      const visibleVehicleMarkers = this.vehicleMarkers$.value.filter(
+        (marker) => bounds.contains(marker.position)
+      );
+
+      this.visibleStopMarkers$.next(visibleStopMarkers);
+    } else {
+      this.visibleStopMarkers$.next([]);
+    }
   }
 
   ngOnDestroy() {
--- frontend/src/app/shared/components/vehicles/vehicles.component.ts
+++ frontend/src/app/shared/components/vehicles/vehicles.component.ts
@@ -7,29 +7,8 @@ import { Vehicle, Vehicles } from '../../../core/models/global.model';
   selector: 'vehicles-component',
   templateUrl: './vehicles.component.html',
 })
-export class VehiclesComponent implements OnInit, OnDestroy {
-  vehicles: Vehicle[] = [];
-  subscription: Subscription = new Subscription();
+export class VehiclesComponent {
+  vehicles$ = this.vehiclesService.vehicles$;
 
   constructor(private vehiclesService: VehiclesService) {}
-
-  ngOnInit(): void {
-    // Initial API fetch of vehicles.
-    this.subscription = this.vehiclesService
-      .getVehiclesObservable()
-      .subscribe((vehicles: Vehicles) => {
-        const newVehicles: Vehicle[] = [];
-        for (const [vehicleNumber, vehicle] of vehicles) {
-          console.log();
-          newVehicles.push(vehicle);
-          this.vehicles = newVehicles;
-        }
-      });
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
 }
