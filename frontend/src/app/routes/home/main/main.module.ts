870dc22eb49b7a3427cf1d7428c4c942a7f53f27
Jeremy Nagahama <naga@hawaii.edu>
2024-08-25 14:05:26 -1000

    setup icons and homepage. add reconnection for websockets.

--- frontend/src/app/routes/home/main/main.module.ts
+++ frontend/src/app/routes/home/main/main.module.ts
@@ -3,11 +3,10 @@ import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
 import { WebsocketService } from '../../../core/services/websocket.service';
 import { GoogleMapComponent } from '../../../shared/components/google-map/google-map.component';
-import { SubscribeComponent } from '../../../shared/components/subscribe-menu/subscribe-menu.component';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [MainRoutingModule, GoogleMapComponent, SubscribeComponent],
+  imports: [MainRoutingModule, GoogleMapComponent],
   providers: [WebsocketService],
 })
 export class MainModule {}
