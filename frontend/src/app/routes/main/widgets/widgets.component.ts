9aad519dd7bec7418a52d20f78bfe1cb9c793ae6
Jeremy Nagahama <naga@hawaii.edu>
2024-10-05 22:53:43 -1000

    add 'favorites nearby' widget

--- frontend/src/app/routes/main/widgets/widgets.component.ts
+++ frontend/src/app/routes/main/widgets/widgets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackedBusComponent } from './tracked-bus/tracked-bus.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @Component({
   selector: 'widgets-component',
-  imports: [CommonModule, TrackedBusComponent],
+  imports: [CommonModule, TrackedBusComponent, FavoritesComponent],
   templateUrl: './widgets.component.html',
   standalone: true,
 })
