e9818d4ea7ea83aa4d36999bb5b1d50c7a6857a8
Jeremy Nagahama <naga@hawaii.edu>
2024-10-13 22:34:40 -1000

    cleanup menu

--- frontend/src/app/routes/main/main.component.ts
+++ frontend/src/app/routes/main/main.component.ts
@@ -1,34 +1,47 @@
 import {
   Component,
   AfterViewInit,
-  ViewContainerRef,
   Injector,
+  ViewChild,
+  ViewContainerRef,
 } from '@angular/core';
 import { WebsocketService } from '../../core/services/websocket.service';
 import { GoogleMapComponent } from './google-map/google-map.component';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { UserDataService } from '../../core/services/user-data.service';
+import { CommonModule } from '@angular/common';
+import { tap } from 'rxjs';
 
 @Component({
   standalone: true,
   templateUrl: './main.component.html',
-  imports: [HeaderComponent, GoogleMapComponent],
+  imports: [HeaderComponent, GoogleMapComponent, CommonModule],
   providers: [WebsocketService],
 })
 export class MainComponent implements AfterViewInit {
   constructor(
-    private viewContainerRef: ViewContainerRef,
-    private injector: Injector
+    private injector: Injector,
+    private userDataService: UserDataService
   ) {}
+
+  @ViewChild('menuComponent', { read: ViewContainerRef })
+  menuContainer!: ViewContainerRef;
+  @ViewChild('widgetsComponent', { read: ViewContainerRef })
+  widgetsContainer!: ViewContainerRef;
+
+  sidebarMode$ = this.userDataService.sidebarMode$.pipe(
+    tap((data) => console.log(data))
+  );
 
   ngAfterViewInit() {
     import('./widgets/widgets.component').then(({ WidgetsComponent }) => {
-      this.viewContainerRef.createComponent(WidgetsComponent, {
+      this.widgetsContainer.createComponent(WidgetsComponent, {
         injector: this.injector,
       });
     });
 
     import('./menu/menu.component').then(({ MenuComponent }) => {
-      this.viewContainerRef.createComponent(MenuComponent, {
+      this.menuContainer.createComponent(MenuComponent, {
         injector: this.injector,
       });
     });
