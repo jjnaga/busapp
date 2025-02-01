534c942e7c04a38def93a69895094ac62f326930
Jeremy Nagahama <naga@hawaii.edu>
2025-02-01 07:54:59 -1000

    ngrx: add user feature

--- frontend/src/app/core/state/state.module.ts
+++ frontend/src/app/core/state/state.module.ts
@@ -4,11 +4,13 @@ import { EffectsModule } from '@ngrx/effects';
 import { rootEffects } from './root.effects';
 import { vehiclesReducer } from './lib/vehicles/vehicles.reducers';
 import { stopsReducer } from './lib/stops/stops.reducers';
+import { userReducer } from './lib/user/user.reducers';
 
 @NgModule({
   imports: [
     StoreModule.forFeature('vehicles', vehiclesReducer),
     StoreModule.forFeature('stops', stopsReducer),
+    StoreModule.forFeature('user', userReducer),
     EffectsModule.forFeature(rootEffects),
   ],
 })
