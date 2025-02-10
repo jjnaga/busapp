2799ec3ea042a7aceb84b0284b6cdfb529da38c7
Jeremy Nagahama <naga@hawaii.edu>
2025-02-09 20:55:36 -1000

    add dynamic map resizing based on drawer height

--- frontend/src/app/core/utils/utils.ts
+++ frontend/src/app/core/utils/utils.ts
@@ -22,3 +22,16 @@ export function getBaseUrl(toastr?: ToastrService, protocol?: string): string {
 
   return url;
 }
+
+export function getVisibleHeight(element: Element) {
+  const rect = element.getBoundingClientRect();
+  const isExpanded = element.classList.contains('translate-y-0');
+
+  // If the drawer is not expanded, return 40% of the height
+  // from the translateY class, that can be abstracted
+  if (!isExpanded) {
+    return rect.height * 0.4;
+  }
+
+  return rect.height;
+}
